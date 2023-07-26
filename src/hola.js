e52328f755808ea8eff57e56c2d8d1abbc145909
grimaldo.soto <Grimaldo.SOTO@Outlook.com>
2023-07-26 12:35:34 -0500

    El compilador de TypeScript

---
+++ src/hola.js
@@ -0,0 +1,4 @@
+var saludo = 'Bienvenido a TypeScript';
+console.log(saludo);
+var fecha = '27/08/2019';
+console.log('fecha', fecha);
---
+++ src/hola.ts
@@ -0,0 +1,5 @@
+let saludo = 'Bienvenido a TypeScript';
+console.log(saludo);
+
+let fecha = '27/08/2019';
+console.log('fecha', fecha);
