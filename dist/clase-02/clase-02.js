e45b8af9e82f726e93eba42a723667915533d855
grimaldo.soto <Grimaldo.SOTO@Outlook.com>
2023-07-28 13:05:36 -0500

    Tipo number

--- dist/clase-02/clase-02.js
+++ dist/clase-02/clase-02.js
@@ -11,3 +11,25 @@ function tieneDescuento(curso) {
     return false;
 }
 console.log(`tieneDescuento('angular'):`, tieneDescuento('angular'));
+//Number
+let estudiantes = 100;
+estudiantes = '100'; //Invalido
+let cantidadEstudiantes;
+cantidadEstudiantes = 120;
+function getCantidadEstudiantes(curso) {
+    if (curso === 'typescript')
+        return 100;
+    return 0;
+}
+let inscritosCursoTypescript = 100;
+//Type: Number, Hexadecimales
+let decimal = 10;
+let hexadecimal = 0xf00d;
+console.log('decimal ', decimal);
+console.log('hexadecimal ', hexadecimal);
+//Type: number, binario
+let binario = 0b1010;
+console.log('binario', binario);
+//type: number, octal
+let octal = 0o755;
+console.log('octal ', octal);
--- src/clase-02/clase-02.ts
+++ src/clase-02/clase-02.ts
@@ -12,3 +12,32 @@ function tieneDescuento(curso){
 }
 
 console.log(`tieneDescuento('angular'):`,tieneDescuento('angular'));
+
+
+//Number
+let estudiantes = 100;
+estudiantes = '100'; //Invalido
+let cantidadEstudiantes: number;
+cantidadEstudiantes = 120;
+
+function getCantidadEstudiantes(curso): number{
+    if(curso === 'typescript')
+        return 100;
+    return 0;
+}
+
+let inscritosCursoTypescript: number = 100;
+
+//Type: Number, Hexadecimales
+let decimal: number = 10;
+let hexadecimal: number = 0xf00d;
+console.log('decimal ', decimal);
+console.log('hexadecimal ', hexadecimal);
+
+//Type: number, binario
+let binario: number = 0b1010;
+console.log('binario', binario);
+
+//type: number, octal
+let octal: number = 0o755;
+console.log('octal ', octal)
