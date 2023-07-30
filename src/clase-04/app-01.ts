84d87a3720bd17e928468c24412e7f14fa1f3d01
grimaldo.soto <Grimaldo.SOTO@Outlook.com>
2023-07-30 16:07:42 -0500

    Clases y constructores

--- src/clase-04/app-01.ts
+++ src/clase-04/app-01.ts
@@ -1,16 +1,23 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class EscuelaDigital {
-    constructor(nombre) {
-        this.cursos = [];
+export {};
+
+class EscuelaDigital{
+
+    //atributos
+    nombre : string;
+    cursos: string [] = [];
+
+    constructor(nombre: string) {
         this.nombre = nombre;
     }
-    agregarCurso(curso) {
+
+    agregarCurso(curso: string):void{
         this.cursos.push(curso);
     }
 }
+
 const escuela = new EscuelaDigital('EDteam');
 escuela.agregarCurso('JavaScript');
 escuela.agregarCurso("TypeScript");
 console.log(escuela);
 console.log(escuela.nombre);
+
