9a8657b48ca122faa9752e947d58a11c9c41453d
grimaldo.soto <Grimaldo.SOTO@Outlook.com>
2023-07-29 20:39:48 -0500

    Creando alias para tipos de datos

---
+++ dist/clase-03/app-01.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// ALIAS PARA TIPOS DE DATOS EN TS
+let curso = 'TypeScript';
+// Cursos codigo: JavaScript = 1, Type = 2
+let cursoNuevo;
+// cursoNuevo = true;
+cursoNuevo = 'JavaScript';
+cursoNuevo = 2; // TypeScript
+function getCurso() {
+    // return 'JavaScript';
+    return 1;
+}
+//arreglos
+let cursos = ['JavaScript', 'TypeScript'];
+let cursos2 = ['JavaScript', 'TypeScript', 1];
+console.log('cursos', cursos);
+console.log('cursos2', cursos2);
