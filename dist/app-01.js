b76a26c51276f7b1a6cf892b1c7a2ef7074c68da
grimaldo.soto <Grimaldo.SOTO@Outlook.com>
2023-07-31 21:28:23 -0500

    Módulos, imports y exports

--- dist/app-01.js
+++ dist/app-01.js
@@ -1,82 +1,13 @@
 "use strict";
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-//DECORADORES
-function Entidad(config) {
-    console.log("Entidad", config);
-    return function (target) {
-        //Dinamicamente: asignamos la propiedad clave a la clase
-        target.clave = config.clave;
-        console.log("target", target);
-    };
-}
-function enumerable(value) {
-    return function (target, propertyKey, descriptor) {
-        descriptor.enumerable = value;
-    };
-}
-function readonly(target, name, descriptor) {
-    descriptor.writable = false;
-    return descriptor;
-}
-let Curso = class Curso {
-    constructor(_id, _nombre) {
-        this._id = _id;
-        this._nombre = _nombre;
-        this.prueba = "";
-    }
-    getPrueba() {
-        return this.prueba;
-    }
-    lectura() { }
-    get id() {
-        return this._id;
-    }
-    set id(id) {
-        this._id = id;
-    }
-    get nombre() {
-        return this._nombre;
-    }
-    set nombre(nombre) {
-        this._nombre = nombre;
-    }
-};
-__decorate([
-    enumerable(true) // no aparece como propiedad
-], Curso.prototype, "getPrueba", null);
-__decorate([
-    readonly
-], Curso.prototype, "lectura", null);
-Curso = __decorate([
-    Entidad({
-        clave: "CURSO",
-    })
-], Curso);
-let EscuelaDigital = class EscuelaDigital {
-};
-EscuelaDigital = __decorate([
-    Entidad({
-        clave: "ESCUELA_DIGITAL",
-    })
-], EscuelaDigital);
-function analizarClase(clase) {
-    console.log("clave:", clase.clave);
-}
-function mostrarPropiedades(clase) {
-    for (let prop in clase.prototipe) {
-        console.log("prop", prop);
-    }
-}
-let typescript = new Curso(1, "TypeScript");
-analizarClase(Curso); //parametro: una clase
+const curso_1 = require("./modelo/curso");
+const escuela_digital_1 = require("./modelo/escuela-digital");
+const util_1 = require("./util");
+let typescript = new curso_1.Curso(1, "TypeScript");
+(0, util_1.analizarClase)(curso_1.Curso); //parametro: una clase
 //sobrescritura de la function
 // typescript.lectura = function() {}; //error
-let escuela = new EscuelaDigital();
-analizarClase(EscuelaDigital);
-mostrarPropiedades(Curso);
+let escuela = new escuela_digital_1.EscuelaDigital();
+(0, util_1.analizarClase)(escuela_digital_1.EscuelaDigital);
+(0, util_1.mostrarPropiedades)(curso_1.Curso);
+console.log('valor de PI:', util_1.PI);
--- src/clase-05/app-01.ts
+++ src/clase-05/app-01.ts
@@ -1,72 +1,10 @@
 export {};
-//DECORADORES
 
-function Entidad(config: any) {
-  console.log("Entidad", config);
+import { Curso} from './modelo/curso';
+import { EscuelaDigital } from './modelo/escuela-digital';
+import { analizarClase, mostrarPropiedades , PI} from "./util";
 
-  return function (target: any) {
-    //Dinamicamente: asignamos la propiedad clave a la clase
-    target.clave = config.clave;
-    console.log("target", target);
-  };
-}
 
-function enumerable(value: boolean) {
-  return function (
-    target: any,
-    propertyKey: string,
-    descriptor: PropertyDescriptor
-  ) {
-    descriptor.enumerable = value;
-  };
-}
-function readonly(target: any, name: any, descriptor: PropertyDescriptor) {
-  descriptor.writable = false;
-  return descriptor;
-}
-
-@Entidad({
-  clave: "CURSO",
-})
-class Curso {
-  prueba: string;
-  constructor(private _id: number, private _nombre: string) {
-    this.prueba = "";
-  }
-
-  @enumerable(true) // no aparece como propiedad
-  getPrueba() {
-    return this.prueba;
-  }
-  @readonly
-  lectura() {}
-  get id() {
-    return this._id;
-  }
-  set id(id: number) {
-    this._id = id;
-  }
-  get nombre() {
-    return this._nombre;
-  }
-  set nombre(nombre: string) {
-    this._nombre = nombre;
-  }
-}
-@Entidad({
-  clave: "ESCUELA_DIGITAL",
-})
-class EscuelaDigital {}
-
-function analizarClase(clase: any) {
-  console.log("clave:", clase.clave);
-}
-
-function mostrarPropiedades(clase: any) {
-  for (let prop in clase.prototipe) {
-    console.log("prop", prop);
-  }
-}
 
 let typescript = new Curso(1, "TypeScript");
 analizarClase(Curso); //parametro: una clase
@@ -76,3 +14,4 @@ let escuela = new EscuelaDigital();
 analizarClase(EscuelaDigital);
 mostrarPropiedades(Curso);
 
+console.log('valor de PI:', PI)
